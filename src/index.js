391b2526857f37334c26ad2cd9d68d47eb76b14f
metalfrenzy747 <allenjeremy747@gmail.com>
2024-01-18 19:57:32 -0500

    basic lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form= document.querySelector('#create-task-form')
+    document.getElementById('create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    taskList(e.target.elements['new-task-description'].value)
+    form.reset()
+  })
 });
+function taskList(tasks) {
+  let ol = document.getElementById('tasks')
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+
+  btn.textContent = 'x';
+  li.textContent = `${tasks} `
+  li.appendChild(btn)
+  ol.appendChild(li)
+
+  btn.addEventListener('click', () => {
+    ol.removeChild(li);
+  })
+}
